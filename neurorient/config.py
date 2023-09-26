6105435dc9b776af03a778b341998d9bb49a6956
carbonscott <wangimagine@gmail.com>
2023-09-26 12:08:20 -0700

    Change the internal config from CONFIG to _CONFIG.

--- neurorient/config.py
+++ neurorient/config.py
@@ -1,21 +1,21 @@
 from .configurator import Configurator
 
-CONFIG = Configurator()
-with CONFIG.enable_auto_create():
-    CONFIG.BACKBONE.RES_TYPE       = 50
+_CONFIG = Configurator()
+with _CONFIG.enable_auto_create():
+    _CONFIG.BACKBONE.RES_TYPE       = 50
 
-    CONFIG.BIFPN.NUM_BLOCKS        = 1
-    CONFIG.BIFPN.NUM_FEATURES      = 64
-    CONFIG.BIFPN.NUM_LEVELS        = 5
-    CONFIG.BIFPN.BN.EPS            = 1e-5
-    CONFIG.BIFPN.BN.MOMENTUM       = 1e-1
-    CONFIG.BIFPN.RELU_INPLACE      = False
-    CONFIG.BIFPN.DOWN_SCALE_FACTOR = 0.5
-    CONFIG.BIFPN.UP_SCALE_FACTOR   = 2
-    CONFIG.BIFPN.FUSION.EPS        = 1e-5
+    _CONFIG.BIFPN.NUM_BLOCKS        = 1
+    _CONFIG.BIFPN.NUM_FEATURES      = 64
+    _CONFIG.BIFPN.NUM_LEVELS        = 5
+    _CONFIG.BIFPN.BN.EPS            = 1e-5
+    _CONFIG.BIFPN.BN.MOMENTUM       = 1e-1
+    _CONFIG.BIFPN.RELU_INPLACE      = False
+    _CONFIG.BIFPN.DOWN_SCALE_FACTOR = 0.5
+    _CONFIG.BIFPN.UP_SCALE_FACTOR   = 2
+    _CONFIG.BIFPN.FUSION.EPS        = 1e-5
 
     # [TODO] Need to set up resnet18 as well
-    CONFIG.BACKBONE.OUTPUT_CHANNELS = {
+    _CONFIG.BACKBONE.OUTPUT_CHANNELS = {
         "relu"   : 64,
         "layer1" : 256,
         "layer2" : 512,
@@ -23,9 +23,9 @@ with CONFIG.enable_auto_create():
         "layer4" : 2048,
     }
 
-    CONFIG.REGRESSOR_HEAD.IN_FEATURES  = CONFIG.BIFPN.NUM_FEATURES * 64 * 64
-    CONFIG.REGRESSOR_HEAD.OUT_FEATURES = 6
+    _CONFIG.REGRESSOR_HEAD.IN_FEATURES  = _CONFIG.BIFPN.NUM_FEATURES * 64 * 64
+    _CONFIG.REGRESSOR_HEAD.OUT_FEATURES = 6
 
-    CONFIG.LOSS_SCALE_FACTOR = 10
+    _CONFIG.LOSS_SCALE_FACTOR = 10
 
-    CONFIG.RESNET2ROTMAT.SCALE = 0
+    _CONFIG.RESNET2ROTMAT.SCALE = 0
