da0c8c41b769ac9b8d09bfd07e5193d68d1f7b56
Zhantao <zhantao@stanford.edu>
2023-10-02 22:26:58 -0700

    remove uses_norm in dataset

--- neurorient/dataset.py
+++ neurorient/dataset.py
@@ -4,9 +4,8 @@ from torch.utils.data import Dataset
 
 
 class TensorDatasetWithTransform(Dataset):
-    def __init__(self, dataset, transform_list = None, uses_norm = True):
+    def __init__(self, dataset, transform_list = None):
         self.dataset        = dataset
-        self.uses_norm      = uses_norm
         self.transform_list = transform_list
 
         self.eps = 1e-6
@@ -21,17 +20,9 @@ class TensorDatasetWithTransform(Dataset):
     def __getitem__(self, idx):
         img = self.dataset[idx]    # (C, H, W)
 
-        img_transformed = img.copy()
+        img_transformed = img.clone()
         for transform in self.transform_list:
             if transform is not None:
                 img_transformed = transform(img_transformed)
-
-        if self.uses_norm:
-            img_transformed_mean = np.nanmean(img_transformed)
-            img_transformed_std  = np.nanstd(img_transformed)
-
-            img_transformed = (img_transformed - img_transformed_mean) / img_transformed_std \
-                  if img_transformed_std > self.eps else \
-                  np.random.rand(*img_transformed.shape)
 
         return (img_transformed, img)    # (C, H, W)
