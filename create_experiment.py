0d302458810d6a6ff7618b12e6a05e862f17f0d2
carbonscott <wangimagine@gmail.com>
2023-10-16 17:59:31 -0700

    Allows to use shorter option.

--- create_experiment.py
+++ create_experiment.py
@@ -8,19 +8,19 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Generate experiment scripts from templates.")
 parser.add_argument("--pdb", required=True, help="Value for PDB.")
-parser.add_argument("--F", action='store_true', default=False, help="Value for USES_PHOTON_FLUCTUATION.")
-parser.add_argument("--P", action='store_true', default=False, help="Value for USES_POISSON_NOISE.")
-parser.add_argument("--G", action='store_true', default=False, help="Value for USES_GAUSSIAN_NOISE.")
-parser.add_argument("--B", action='store_true', default=False, help="Value for USES_BEAM_STOP_MASK.")
-parser.add_argument("--C", action='store_true', default=False, help="Value for USES_RANDOM_PATCH.")
+parser.add_argument("-F", action='store_true', default=False, help="Value for USES_PHOTON_FLUCTUATION.")
+parser.add_argument("-P", action='store_true', default=False, help="Value for USES_POISSON_NOISE.")
+parser.add_argument("-G", action='store_true', default=False, help="Value for USES_GAUSSIAN_NOISE.")
+parser.add_argument("-B", action='store_true', default=False, help="Value for USES_BEAM_STOP_MASK.")
+parser.add_argument("-C", action='store_true', default=False, help="Value for USES_RANDOM_PATCH.")
 args = parser.parse_args()
 
-pdb       = args.pdb
-F         = args.F
-P         = args.P
-G         = args.G
-B         = args.B
-C         = args.C
+pdb = args.pdb
+F   = args.F
+P   = args.P
+G   = args.G
+B   = args.B
+C   = args.C
 
 # Set up the environment and the loader
 env = Environment(loader=FileSystemLoader('.'))
